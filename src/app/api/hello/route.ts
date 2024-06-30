edd0d312cab4bd8c120b73761d461544f82d9ebb
arre_ankit <arre.ankit@gmail.com>
2024-07-01 01:27:56 +0530

    intialzed repo

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,22 +1,10 @@
 import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
 
 export const runtime = 'edge'
 
+// route.ts
 export async function GET(request: NextRequest) {
-  let responseText = 'Hello World'
-
-  // In the edge runtime you can use Bindings that are available in your application
-  // (for more details see:
-  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
-  //    - https://developers.cloudflare.com/pages/functions/bindings/
-  // )
-  //
-  // KV Example:
-  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
-
-  return new Response(responseText)
+    const apiResponse = await fetch('https://cf-ai-worker.ankit992827.workers.dev/')
+    const apiResponseText = await apiResponse.text()
+    return new Response(apiResponseText)
 }
