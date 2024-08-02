1eaed30b56f60b0f5ad63e4c15ca083e383fb79c
arre_ankit <arre.ankit@gmail.com>
2024-08-02 14:53:14 +0530

    secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,5 +9,5 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.SECRET
+  secret: process.env.NEXTAUTH_SECRET,
 })
